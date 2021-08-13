6dcecd8f101a957209665d26ab1a50a3a7bc2d55
Bradember <bybeltranc@unal.edu.co>
2021-08-13 12:28:19 -0500

    back_order field.boolean

--- overwrite_inventory/models/models.py
+++ overwrite_inventory/models/models.py
@@ -572,6 +572,8 @@ class Picking(models.Model):
         ('terminadas','Terminadas')
         ], string='Cantidades Terminadas')
 
+    back_order = fields.Boolean()
+
     @api.onchange('create_lot')
     def set_defaut_lot(self):
         if self.create_lot:
@@ -912,6 +914,31 @@ class Picking(models.Model):
                     'date_val': datetime.datetime.now()})
         return
 
+    def _create_backorder(self):
+
+        """ This method is called when the user chose to create a backorder. It will create a new
+        picking, the backorder, and move the stock.moves that are not `done` or `cancel` into it.
+        """
+        backorders = self.env['stock.picking']
+        for picking in self:
+            moves_to_backorder = picking.move_lines.filtered(lambda x: x.state not in ('done', 'cancel'))
+            if moves_to_backorder:
+                backorder_picking = picking.copy({
+                    'name': '/',
+                    'move_lines': [],
+                    'move_line_ids': [],
+                    'backorder_id': picking.id
+                })
+                picking.message_post(
+                    body=_('The backorder <a href=# data-oe-model=stock.picking data-oe-id=%d>%s</a> has been created.') % (
+                        backorder_picking.id, backorder_picking.name))
+                moves_to_backorder.write({'picking_id': backorder_picking.id})
+                moves_to_backorder.mapped('package_level_id').write({'picking_id':backorder_picking.id})
+                moves_to_backorder.mapped('move_line_ids').write({'picking_id': backorder_picking.id})
+                backorder_picking.action_assign()
+                backorders |= backorder_picking
+                backorders.back_order = True
+        return backorders
 
 class Warehouse(models.Model):
     _inherit = "stock.warehouse"
